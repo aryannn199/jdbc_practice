d5e7e41cc3a836ec04c1655bc8dfa07e83f4c52c
aryannn199 <164353303+aryannn199@users.noreply.github.com>
2025-01-23 09:36:58 +0530

    Update PracticeSorting.java

--- PracticeSorting.java
+++ PracticeSorting.java
@@ -1,54 +1,140 @@
-package SortingAlgortihms;
+package SortingAlgorithms;
 
 import java.util.Arrays;
 
 public class PracticeSorting {
     public static void main(String[] args) {
-        int [] arr = {5,4,3,2,1};
+        int[] arr = {5, 4, 3, 2, 1};
         sort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 
-     public static void sort(int [] arr){
+    public static void sort(int[] arr) {
         SelectionSort(arr);
-     }
+    }
 
-    public static void SelectionSort(int [] arr){
-        for(int i = 0;i< arr.length-1;i++){
+    public static void SelectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int minimumIdx = i;
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[j]<arr[minimumIdx]){
-                    minimumIdx=j;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minimumIdx]) {
+                    minimumIdx = j;
                 }
             }
             int temp = arr[i];
             arr[i] = arr[minimumIdx];
-            arr[minimumIdx]=temp;
-
+            arr[minimumIdx] = temp;
         }
     }
 
-    public static void BubbleSort(int [] arr){
-        for(int i = 0;i< arr.length;i++){
-            for(int j =0;j< arr.length-i-1;j++){
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j]=temp;
+    public static void BubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
     }
 
-    public static void InsertionSort(int [] arr){
-        for(int i =1;i<arr.length;i++){
+    public static void InsertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
             int key = arr[i];
-            int j = i-1;
-            while(j>=0 && arr[j]>key){
-                arr[j+1] = arr[j];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
                 j--;
             }
-            arr[j+1] = key;
+            arr[j + 1] = key;
         }
     }
 
+    public static void MergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            MergeSort(arr, left, mid);
+            MergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
 
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        System.arraycopy(arr, left, L, 0, n1);
+        System.arraycopy(arr, mid + 1, R, 0, n2);
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public static void QuickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            QuickSort(arr, low, pi - 1);
+            QuickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void ShellSort(int[] arr) {
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static int LinearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int BinarySearch(int[] arr, int key) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == key) return mid;
+            if (arr[mid] < key) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
 }
